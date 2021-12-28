ee5ffea7ab3052172e63286d037412480fd9d31c
Tredici <marcolucio1999@google.com>
2021-12-28 19:43:00 +0100

    Add C 32 bit main function

---
+++ kmain32.c
@@ -0,0 +1,15 @@
+#include "video32bit.h"
+#include "helpers_32bit.h"
+
+/**
+ * @brief kernel 32 bit main function
+ * 
+ * @return int 
+ */
+int main32()
+{
+    clear_screen();
+    printline("HELLO PANINARO!!!");
+    printline("Uomo cactus!");
+    return 0;
+}
