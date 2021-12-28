654fddc6c08e5215e27b1f68e5521272a62508a1
Tredici <marcolucio1999@google.com>
2021-12-28 21:02:45 +0100

    Use string functions in kmain32

--- kmain32.c
+++ kmain32.c
@@ -1,5 +1,6 @@
 #include "video32bit.h"
 #include "helpers_32bit.h"
+#include "string32.h"
 
 /**
  * @brief kernel 32 bit main function
@@ -11,5 +12,8 @@ int main32()
     clear_screen();
     printline("HELLO PANINARO!!!");
     printline("Uomo cactus!");
+    printline(itoa32(10000));
+    printline("XXXXXXXX");
+    printline(itoa32(atoi32("")));
     return 0;
 }
