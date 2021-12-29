bc6edb3406f9eda42dc09ca4641c865236aa4cb6
Tredici <marcolucio1999@google.com>
2021-12-29 12:38:52 +0100

    Define explicit lenght types

---
+++ types.h
@@ -0,0 +1,24 @@
+/**
+ * List type definitions used in this project.
+ */
+
+#ifndef TYPES
+#define TYPES
+
+typedef signed char s8;
+typedef unsigned char u8;
+typedef u8 byte;
+
+typedef signed short s16;
+typedef unsigned short u16;
+typedef u16 word;
+
+typedef int s32;
+typedef unsigned int u32;
+typedef u32 doubleword;
+
+typedef long long int s64;
+typedef unsigned long long int u64;
+typedef u64 quadword;
+
+#endif
