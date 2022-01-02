bccbef9ef01ae2285e3dafc436480a8de1422f67
Tredici <marcolucio1999@google.com>
2022-01-02 04:13:42 +0100

    Print data from multiboot structure

--- kmain32.c
+++ kmain32.c
@@ -1,15 +1,57 @@
 #include "video32bit.h"
 #include "helpers_32bit.h"
 #include "string32.h"
+#include "multiboot.h"
+
+/**
+ * @brief Display multiboot info passed by the
+ * bootloader
+ *
+ * @param mi struct multiboot_info
+ */
+void print_multiboot(struct multiboot_info *mi)
+{
+    putstr("FLAGS: "); puti(mi->flags); newline();
+    if (mi->flags & MULTIBOOT_FLAG_0)
+    {
+        putstr("MEMORY lower: "); puti(mi->mem_lower); putc('K'); newline();
+        putstr("MEMORY upper: "); puti(mi->mem_upper); putc('K'); newline();
+    }
+    else printline("MEMORY info not availabe!");
+
+    putstr("CMDLINE:      ");
+    if (mi->flags & MULTIBOOT_FLAG_2)   printline(mi->cmdline);
+    else    printline("(Unspecified!)");
+
+    if (mi->flags & MULTIBOOT_FLAG_5)
+    {
+        printline("ELF Kernel!");
+    }
+
+    putstr("BOOTLOADER:   ");
+    if (mi->flags & MULTIBOOT_FLAG_9)
+        printline(mi->boot_loader_name);
+    else
+        printline("(Unspecified!)");
+
+    if (mi->flags & MULTIBOOT_FLAG_11)
+        printline("VBE info available!");
+    else
+        printline("VBE info NOT available!");
+}
 
 /**
  * @brief kernel 32 bit main function
  * 
  * @return int 
  */
-int main32()
+int main32(struct multiboot_info *mi)
 {
     clear_screen();
+    putstr("Multiboot info: "); puti((int)mi); newline();
+    if (mi)
+        print_multiboot(mi);
+
     printline("HELLO PANINARO!!!");
     printline("Uomo cactus!");
     printline(itoa32(10000));
