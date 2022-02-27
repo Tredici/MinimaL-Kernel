1389c964e2b56866e2d87cf9ece9da5ef5dac9b0
Tredici <marcolucio1999@google.com>
2022-02-27 22:38:00 +0100

    Call start_vm from main64

--- kmain64.c
+++ kmain64.c
@@ -3,6 +3,7 @@
  */
 
 #include "video64bit.h"
+#include "vm64.h"
 
 void main64()
 {
@@ -10,5 +11,6 @@ void main64()
     printline64("Hello 64 bit!");
     printline64("Long Mode Activated!");
     printline64("Good Bye!");
+    start_vm();
 }
 
