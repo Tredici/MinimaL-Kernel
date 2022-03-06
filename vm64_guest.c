2fbad3ab5a8d22a612f66a6a972a48bc1a2af8df
Tredici <marcolucio1999@google.com>
2022-03-06 21:53:36 +0100

    Add function to read VMCS 32b ro fields
    See [Table B-9. Encodings for 32-Bit Read-Only Data Fields (0100_01xx_xxxx_xxx0B)] for details

--- vm64_guest.c
+++ vm64_guest.c
@@ -95,3 +95,84 @@ void vmx_guestw_itrd_gs(short val)
 {
     vmx_write_vmcs_field(GUEST_GS_SELECTOR, val);
 }
+
+/**
+ * Following fields are READ ONLY!!!!
+ *
+ * See Intel Manual Vol. 3
+ *  [Table B-9. Encodings for 32-Bit Read-Only Data Fields (0100_01xx_xxxx_xxx0B)]
+ *  Field Name                          Index       Encoding
+ *  VM-instruction error                000000000B  00004400H
+ *  Exit reason                         000000001B  00004402H
+ *  VM-exit interruption information    000000010B  00004404H
+ *  VM-exit interruption error code     000000011B  00004406H
+ *  IDT-vectoring information field     000000100B  00004408H
+ *  IDT-vectoring error code            000000101B  0000440AH
+ *  VM-exit instruction length          000000110B  0000440CH
+ *  VM-exit instruction information     000000111B  0000440EH
+ */
+#define GUEST_VM_INSTRUCTION_ERROR                0x00004400
+#define GUEST_EXIT_REASON                         0x00004402
+#define GUEST_VM_EXIT_INTERRUPTION_INFORMATION    0x00004404
+#define GUEST_VM_EXIT_INTERRUPTION_ERROR_CODE     0x00004406
+#define GUEST_IDT_VECTORING_INFORMATION_FIELD     0x00004408
+#define GUEST_IDT_VECTORING_ERROR_CODE            0x0000440A
+#define GUEST_VM_EXIT_INSTRUCTION_LENGTH          0x0000440C
+#define GUEST_VM_EXIT_INSTRUCTION_INFORMATION     0x0000440E
+
+int vmx_guest_read_vm_instruction_error()
+{
+    long ans;
+    vmx_read_vmcs_field(&ans, GUEST_VM_INSTRUCTION_ERROR);
+    return ans;
+}
+
+int vmx_guest_read_vm_exit_reason()
+{
+    long ans;
+    vmx_read_vmcs_field(&ans, GUEST_EXIT_REASON);
+    return ans;
+}
+
+int vmx_guest_read_vm_exit_interruption_information()
+{
+    long ans;
+    vmx_read_vmcs_field(&ans, GUEST_VM_EXIT_INTERRUPTION_INFORMATION);
+    return ans;
+}
+
+int vmx_guest_read_vm_exit_interruption_error_code()
+{
+    long ans;
+    vmx_read_vmcs_field(&ans, GUEST_VM_EXIT_INTERRUPTION_ERROR_CODE);
+    return ans;
+}
+
+int vmx_guest_read_idt_vectoring_information_field()
+{
+    long ans;
+    vmx_read_vmcs_field(&ans, GUEST_IDT_VECTORING_INFORMATION_FIELD);
+    return ans;
+}
+
+int vmx_guest_read_guest_idt_vectoring_error_code()
+{
+    long ans;
+    vmx_read_vmcs_field(&ans, GUEST_IDT_VECTORING_ERROR_CODE);
+    return ans;
+}
+
+int vmx_guest_read_guest_vm_exit_instruction_length()
+{
+    long ans;
+    vmx_read_vmcs_field(&ans, GUEST_VM_EXIT_INSTRUCTION_LENGTH);
+    return ans;
+}
+
+int vmx_guest_read_guest_vm_exit_instruction_information()
+{
+    long ans;
+    vmx_read_vmcs_field(&ans, GUEST_VM_EXIT_INSTRUCTION_INFORMATION);
+    return ans;
+}
+
--- vm64_guest.h
+++ vm64_guest.h
@@ -16,4 +16,13 @@ void vmx_guest_write_es(short val);
 void vmx_guest_write_fs(short val);
 void vmx_guestw_itrd_gs(short val);
 
+int vmx_guest_read_vm_instruction_error();
+int vmx_guest_read_vm_exit_reason();
+int vmx_guest_read_vm_exit_interruption_information();
+int vmx_guest_read_vm_exit_interruption_error_code();
+int vmx_guest_read_idt_vectoring_information_field();
+int vmx_guest_read_guest_idt_vectoring_error_code();
+int vmx_guest_read_guest_vm_exit_instruction_length();
+int vmx_guest_read_guest_vm_exit_instruction_information();
+
 #endif
