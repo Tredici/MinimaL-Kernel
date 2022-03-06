cb268e4751b31ab6670ba2883eb2bd28240bd933
Tredici <marcolucio1999@google.com>
2022-03-06 18:34:45 +0100

    Add functions to read segment registers

--- status_operations64.h
+++ status_operations64.h
@@ -15,4 +15,15 @@ long so_read_cr6();
 long so_read_cr7();
 long so_read_cr8();
 
+/**
+ * Functions to read segment registers
+ */
+short so_read_cs();
+short so_read_ds();
+short so_read_ss();
+short so_read_es();
+short so_read_fs();
+short so_read_gs();
+
+
 #endif
