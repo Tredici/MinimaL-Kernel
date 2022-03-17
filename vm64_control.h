22783801a274c706462b6360461b4c24cb1d7773
Tredici <marcolucio1999@google.com>
2022-03-17 10:37:57 +0100

    Fix comments

--- vm64_control.h
+++ vm64_control.h
@@ -82,6 +82,7 @@ void vmx_write_cr3_target_value_1(long val);
 void vmx_write_cr3_target_value_2(long val);
 void vmx_write_cr3_target_value_3(long val);
 
+
 /* Read Only Natural width fields */
 long vmx_read_exit_qualification();
 long vmx_read_io_rcx();
