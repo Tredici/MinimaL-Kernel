7c090d10b08024f8deb9b6ebb1b5101663f665f0
Tredici <marcolucio1999@google.com>
2022-03-30 18:38:46 +0200

    Improve interrupt handlers

--- interrupt64_handlers.c
+++ interrupt64_handlers.c
@@ -7,40 +7,53 @@
 
 void handle_div0_c()
 {
+    set_background_color(7);
+    set_foreground_color(8);
     print_current_task_status();
     panic64("Exception DIV0!");
 }
 
 void handle_int3_c()
 {
+    set_background_color(7);
+    set_foreground_color(8);
     print_current_task_status();
     panic64("Exception Breakpoint!");
 }
 
 void handle_ud_c(void *rip)
 {
+    set_background_color(7);
+    set_foreground_color(8);
     print_current_task_status();
     char buffer[80] = "#UD(";
     char *tmp;
     tmp = strcat64(buffer, hex64((long)rip));
-    tmp = strcat64(tmp, ")");
+    tmp = strcat64(tmp, ") ");
+    tmp = strcat64(tmp, hex64(*(long*)rip));
     panic64(buffer);
 }
 
 void handle_double_f_c()
 {
+    set_background_color(7);
+    set_foreground_color(8);
     print_current_task_status();
     panic64("Double fault!");
 }
 
 void handle_stack_f_c()
 {
+    set_background_color(7);
+    set_foreground_color(8);
     print_current_task_status();
     panic64("Stack fault!");
 }
 
 void handle_gpe_c(long error)
 {
+    set_background_color(7);
+    set_foreground_color(8);
     print_current_task_status();
     char buffer[80] = "General Protection Exception(";
     char *tmp;
@@ -51,18 +64,24 @@ void handle_gpe_c(long error)
 
 void handle_tss_c()
 {
+    set_background_color(7);
+    set_foreground_color(8);
     print_current_task_status();
     panic64("Exception TSS!");
 }
 
 void handle_snp_c()
 {
+    set_background_color(7);
+    set_foreground_color(8);
     print_current_task_status();
     panic64("Exception SNP!");
 }
 
 void handle_pfe_c()
 {
+    set_background_color(7);
+    set_foreground_color(8);
     print_current_task_status();
     panic64("Page fault!");
 }
